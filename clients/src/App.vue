23f86979f19215565dfaf28e1a336c3c66599b17
Jonathan Baez <jbaez9910@gmail.com>
2021-02-03 13:05:33 -0500

    Mostrar data del backend en el frontend

--- clients/src/App.vue
+++ clients/src/App.vue
@@ -2,8 +2,16 @@
   <div>
     <div class="container p-4">
       <div class="row">
-        <div class="col-md-4">
-
+        
+        <div class="col-md-4" v-for="article in articles" :key="article._id">
+       
+          <div class="card mb-2">
+            <img src="./assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5>{{article.title}}</h5>
+              <p>{{article.description}}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -27,8 +35,7 @@ export default {
   methods:{ 
     async getArticles(){
      const res = await this.axios.get(`${this.baseUrl}/articles`);
-     //console.log(`${this.baseUrl}/articles`)
-     console.log(res);
+     this.articles=res.data.articles
     }
   }
 }
