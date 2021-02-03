4a6a6f2e411cce7c4ebd58c573b2c6d7617f42d8
Jonathan Baez <jbaez9910@gmail.com>
2021-02-03 12:39:34 -0500

    correcion frontend, y traer datos de prueba por consola

---
+++ clients/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-md-4">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      articles : [],
+      baseUrl:'http://localhost:4000',
+      page: 1,
+      perPage: 10,
+      pages: []
+    }
+  },
+  created(){
+    this.getArticles();
+  },
+  methods:{ 
+    async getArticles(){
+     const res = await this.axios.get(`${this.baseUrl}/articles`);
+     //console.log(`${this.baseUrl}/articles`)
+     console.log(res);
+    }
+  }
+}
+</script>
